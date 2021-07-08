4e8038265349d973387a1304333b855bc0bca94a
Lindow Amamiya <76984961+mccnlight@users.noreply.github.com>
2021-07-09 01:50:37 +0700

    biar rapih aja

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const chalk = require("chalk");
 
 const { setppig, login, logout, follow, upinstastory, unfollow } = require("./lib/handlerIg.js");
 const { uptotele, uptonaufal, uploadFile } = require('./lib/uploadimage');
+const { menu } = require("./lib/menu")
 const conn = require("./lib/connect");
 
 conn.connect()
@@ -134,40 +135,7 @@ lintod.on('chat-update', async(lin) => {
           await upinstastory(uplii, ini, from, prefix) 
           break
           case "help":
-          reply(`Simple selfbot
-  
-Author : lintod
-  
-[ Instagram Tools ]
-
-Note : Harap pakai ${prefix}logininsta dahulu sebelum memakai fitur Instagram
-
-1. *${prefix}logininsta*
-Untuk login Instagram
-Usage : ${prefix}logininsta
-
-2. *${prefix}logoutinsta*
-Untuk login Instagram
-Usage : ${prefix}logininsta
-
-3. *${prefix}setppig*
-Untuk mengubah profile picture Instagram
-Usage : ${prefix}setppig
-
-4. *${prefix}upinstastory*
-Untuk upstory image
-Usage : ${prefix}upinstastory <reply image>
-
-5. *${prefix}follow*
-Untuk follow user Instagram
-Usage : ${prefix}follow <username>
-
-6. *${prefix}unfollow*
-Untuk unfollow user Instagram
-Usage : ${prefix}unfollow <username>
-
-More? Nanti, tunggu update aja 😁
-`)
+          reply(menu(prefix))
           break
         }
     } catch (e) {
